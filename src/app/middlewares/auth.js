bfc00b8ad0824de6ab9215d1b203dc51e2d069cb
Tiago <tiago.unity.cruz@gmail.com>
2020-04-23 19:47:35 -0300

    auth

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = async (req, res, next) => {
     const authHeader = req.headers.authorization
 
     if(!authHeader){
-        res.status(401).json({error: 'Token not provided'})
+        return res.status(401).json({error: 'Token not provided'})
     }
 
     const [, token] = authHeader.split(' ')
