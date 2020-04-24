481c8e8a83034020f4f79330725bc8777c8d2181
Tiago <tiago.unity.cruz@gmail.com>
2020-04-23 23:31:13 -0300

    html no email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -99,6 +99,10 @@ class AppointmentController{
                 model: User,
                 as: 'provider',
                 attributes: ['name', 'email']
+            },{
+                model: User,
+                as: 'user',
+                attributes: ['name']
             }]
         })
 
@@ -119,7 +123,12 @@ class AppointmentController{
         await Mail.sendMail({
             to: `${appointment.provider.name} <${appointment.provider.email}>`,
             subject: 'Agendamento cancelado',
-            text: 'Você tem um novo cancelamento'
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: appointment.date
+            }
         })
 
         return res.json(appointment)
